83a5446c9936178a66a35cf97d1490fc87264f16
Henry <hsabio.a@gmail.com>
2019-11-08 00:08:38 -0500

    Completed favorite section

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -7,17 +7,18 @@ const firebaseConfig = {
     storageBucket: "scene-it-3f5e8.appspot.com",
     messagingSenderId: "844424365394",
     appId: "1:844424365394:web:9c430cb2816322066a268b"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
 
 let favSearchResponse;
 let favTitle = "";
 let favPoster = "";
 let rating = 0;
+let favPosterLink = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
 
 // Capture Button Click
 $("#add-favorite").on("click", function (event) {
@@ -28,7 +29,6 @@ $("#add-favorite").on("click", function (event) {
     // Begin API Query
     const favInput = $("#fav-input").val().trim();
     console.log(favInput);
-    let moviePosterImg = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
 
     //Defines API query URL
     const queryURL = "https://api.themoviedb.org/3/search/movie?api_key=33d4301a92067b28681045ddd01c67ad&query=" + favInput;
@@ -43,7 +43,7 @@ $("#add-favorite").on("click", function (event) {
             favSearchResponse = fav.results;
             console.log(fav.results);
 
-            favTitle = favSearchResponse[1].title;
+            favTitle = favSearchResponse[0].title;
             favPoster = favSearchResponse[0].poster_path;
             rating = favSearchResponse[0].vote_average;
 
@@ -54,54 +54,54 @@ $("#add-favorite").on("click", function (event) {
                 poster: favPoster,
                 movieRating: rating
             });
-            
+
         });
-        console.log("test");
-        
-    })
-    
-    // Code in the logic for storing and retrieving the most recent user.
-    console.log("database");
-        
+    console.log("test");
 
+})
 
+// Code in the logic for storing and retrieving the most recent user.
+console.log("database");
 
+// Firebase watcher + initial loader HINT: .on("value")
+database.ref().on("child_added", function (snapshot) {
 
-// // Firebase watcher + initial loader HINT: .on("value")
-// database.ref().on("child_added", function (snapshot) {
+    let movieCard = $("<div>");
+    movieCard.attr("id", "fav");
+    movieCard.attr("class", "card fav-card col-sm-2 col-md-2 col-lg-2 fav-card animated delay-1s fadeIn");
 
-//     let movieCard = $("<div>");
-//     movieCard.attr("id", "trending-" + i);
-//     movieCard.attr("class", "card trend-card col-sm-2 col-md-2 col-lg-2 trending-card animated delay-1s fadeIn");
+    let cardRow = $("<div>");
+    cardRow.attr("id", "card-row");
+    cardRow.attr("class", "row fav-row");
 
-//     let cardRow = $("<div>");
-//     cardRow.attr("id", "card-row-" + i);
-//     cardRow.attr("class", "row trending-row");
+    let cardImageContainer = $("<div>");
+    cardImageContainer.attr("id", "card-image-container");
+    cardImageContainer.attr("class", "col-sm-12 col-md-12 col-lg-12 fav-img-container");
 
-//     let cardImageContainer = $("<div>");
-//     cardImageContainer.attr("id", "card-image-container-" + i);
-//     cardImageContainer.attr("class", "col-sm-4 col-md-4 col-lg-4");
+    let cardImage = $("<img>");
+    cardImage.attr("id", "card-image");
+    cardImage.attr("class", "fav-poster");
+    cardImage.attr("src", favPosterLink + snapshot.val().poster);
 
-//     let cardImage = $("<img>");
-//     cardImage.attr("id", "card-image-" + i);
-//     cardImage.attr("class", "movie-poster");
-//     cardImage.attr("src", moviePosterImg + favSearchResponse[trendingMovieIndex].poster_path);
+    let cardBodyContainer = $("<div>");
+    cardBodyContainer.attr("id", "card-body-container");
+    cardBodyContainer.attr("class", "col-sm-12 col-md-12 col-lg-12");
 
-//     let cardBodyContainer = $("<div>");
-//     cardBodyContainer.attr("id", "card-body-container-" + i);
-//     cardBodyContainer.attr("class", "col-sm-8 col-md-8 col-lg-8");
+    let cardBody = $("<div>")
+    cardBody.attr("id", "card-body");
+    cardBody.attr("class", "card-body");
+    cardBody.append("<h4>" + snapshot.val().title + "</h4>");
+    cardBody.append("<p>Average Rating: " + snapshot.val().movieRating + "</p>");
 
-//     let cardBody = $("<div>")
-//     cardBody.attr("id", "card-body-" + i);
-//     cardBody.attr("class", "card-body");
-//     cardBody.append("<h6>" + favSearchResponse[trendingMovieIndex].title + "</h6>");
-//     // cardBody.append("<p>Average Rating" + favSearchResponse[trendingMovieIndex].vote_average + "</p>");
+    movieCard.append(cardRow);
+    cardRow.append(cardImageContainer);
+    cardImageContainer.append(cardImage);
+    cardRow.append(cardBodyContainer);
+    cardBodyContainer.append(cardBody);
 
-//     movieCard.append(cardRow);
-//     cardRow.append(cardImageContainer);
-//     cardImageContainer.append(cardImage);
-//     cardRow.append(cardBodyContainer);
-//     cardBodyContainer.append(cardBody);
+    $("#user-favorites").append(movieCard);
 
-//     $(".user-favorites").append(movieCard);
-// })
+    // $("#favorites-title").append(snapshot.val().title);
+    // $("#favorites-title").append(favPosterLink + snapshot.val().poster);
+    // $("#favorites-title").append(snapshot.val().movieRating);
+})
